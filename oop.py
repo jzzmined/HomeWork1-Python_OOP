44e3d1dfd657fd5b0586bec364ce201198146c9a
jzzmined <j.cadelina.554273@umindanao.edu.ph>
2026-09-07 17:23:37 +0700

    making a methods

---
+++ oop.py
@@ -0,0 +1,11 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+        def bark(self):
+            print("Woof! Woof!")
+
+        def celebrateBirthday(self):
+            self.age += 1
+            print(f"Happy birthday! {self.name} is now {self.age} years old.")
