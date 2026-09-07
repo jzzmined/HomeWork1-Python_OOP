ad64e9e8214741a9fc1a6be60faf718d2c6f305a
jzzmined <j.cadelina.554273@umindanao.edu.ph>
2026-09-07 17:27:07 +0700

    coding the main py

--- oop.py
+++ oop.py
@@ -9,3 +9,15 @@ class Dog:
         def celebrateBirthday(self):
             self.age += 1
             print(f"Happy birthday! {self.name} is now {self.age} years old.")
+
+        def getInfo(self):
+            return f"Dog Name: {self.name}, Age: {self.age}"
+
+
+if __name__ == "__main__":
+    my_dog = Dog("Max", 5)
+    print(my_dog.getInfo())
+    my_dog.bark()
+    my_dog.celebrateBirthday()
+    print(my_dog.getInfo())
+    
